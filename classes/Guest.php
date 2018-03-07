fe821fa746f1d37951bce2f913ee54700d7a7c05
Alex Shornal <shornality@yahoo.com>
2018-03-07 16:01:39 -0500

    installed composer

---
+++ classes/Guest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 3/7/2018
+ * Time: 12:55 PM
+ */
+
+class Guest
+{
+
+}
--- index.php
+++ index.php
@@ -5,3 +5,22 @@
  * Date: 3/7/2018
  * Time: 12:25 PM
  */
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 3);
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+
+
+//Define a default route
+$f3->route('GET /', function()
+{
+    $template = new Template();
+    echo $template->render('pages/home.html');
+}
+);
