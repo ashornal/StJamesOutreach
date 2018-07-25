72308d3aeee75fdaffc6ac1d23a9ac63ff98d3cb
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-07-25 16:16:16 -0400

    pressing delete guest changes value in database

--- js/deleteGuest.js
+++ js/deleteGuest.js
@@ -2,6 +2,8 @@
 
 // adds a listener to window.onload
 $(document).ready(function() {
+    //$("p.text-warning").hide();
+
     $("button#delete").click(function() {
         // navigates to the table row
         var info = $(this).parent().parent().text();
@@ -9,5 +11,19 @@ $(document).ready(function() {
         info = info.trim().split(" ");
         var id = info[0].trim();
         var name = info[20].trim() + " " + info[41].trim();
+        var message = "Are you sure you want to delete " + name + " (#" + id + ")?";
+
+        $("#deleteMessage").html(message);
+        // delete the guest by id
+
+        $("#confirmDelete").click(function() {
+            var hideGuest = $.post('model/hideGuest.php', {id: id});
+
+            // display success, then reload the page
+            hideGuest.done(function () {
+                $("p.text-warning").show();
+                setTimeout(location.reload.bind(location), 1500);
+            });
+        });
     });
 });
